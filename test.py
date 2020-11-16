cce79b1e0599cec4b841515d610db51190c64fad
pzhao22 <pzhao2298@gmail.com>
2020-11-16 13:24:32 -0500

    initial commit

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-name = input("Your name: ")
-print("Hello, ", name)
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+print(r.ok)
